926f385a5e45dc6b3fd7f1d49c11e9cdfd968330
Igor Peymer <ipigp@yandex.ru>
2025-10-03 18:04:34 +0300

    new file:   package-lock.json 	modified:   package.json 	renamed:    test-canvas.html -> public/index.html 	new file:   server.mjs

---
+++ server.mjs
@@ -0,0 +1,31 @@
+import F from 'fastify'
+import staticF from '@fastify/static'
+import cors from '@fastify/cors'
+import path from 'path'
+
+const __dirname = import.meta.dirname;
+
+const server = F({ logger: false })
+server.register(cors, {
+    origin: '*',
+})
+
+server.register(staticF, {
+    root: path.join(__dirname, 'public'),
+    prefix: '/public/', // optional: default '/'
+});
+
+server.get("/", async (req, repl) => {
+    return repl.sendFile('index.html')
+})
+
+server.post("/", async (req, reply) => {
+    let data = JSON.parse(req.body)
+    data.gen++;
+    return JSON.stringify(data)
+})
+
+let s = server.listen({ port: 5555 }, (err, address) => {
+    if (err) throw err
+    console.log(`Работает по адресу ${address}`)
+})
